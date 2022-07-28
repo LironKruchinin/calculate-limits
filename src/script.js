8c9e633caa12d49728c640997aabb7694ea985d3
Liron Kruchinin <54192894+LilKruchi@users.noreply.github.com>
2022-07-28 13:14:08 +0300

    Updated the js and HTML

--- src/script.js
+++ src/script.js
@@ -1,13 +1,30 @@
 const bt = document.querySelector('#bt');
-let output = [];
 exportData.addEventListener('click', (event) => {
+    let output = [];
+    let timeMax = document.getElementById('maxF').value;
+    let timeMin = document.getElementById('minF').value;
+    let removeStart0TimeMin = parseInt(timeMin, 10);
+    let removeStart0TimeMax = parseInt(timeMax, 10);
+
+    if((removeStart0TimeMin == 0) || (removeStart0TimeMin == NaN)){
+        removeStart0TimeMin = null;
+    }
+    if((removeStart0TimeMax == 0) || (removeStart0TimeMin == NaN)){
+        removeStart0TimeMax = null;
+    }
+
+    let textField = document.querySelectorAll('input[name="text"]');
+    
+
     let checkboxes = document.querySelectorAll('input[name="elem"]:checked');
     checkboxes.forEach((checkbox) => {
-        output.push(checkbox.value);
+        output.push([checkbox.value, checkbox.value] );
     });
-    alert(output);
+    let outPutObj = Object.fromEntries(output);
+    console.log(outPutObj);
+    console.log(removeStart0TimeMin);
+    console.log(removeStart0TimeMax);
 });   
-console.log(output);
 
 /*
 do double push into main array, as exampel
